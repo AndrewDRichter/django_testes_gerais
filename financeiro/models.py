5c8a37bee3b8bdd5c962969bda5cf919e5a1d24c
AndrewDRichter <andrewrichter1@hotmail.com>
2024-04-30 16:29:10 -0400

    criação de modelos conta

--- financeiro/models.py
+++ financeiro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Conta(models.Model):
+    nome_conta = models.CharField("Nome da conta", max_length=200)
+    descricao_conta = models.TextField("Descrição da conta")
+    # adicionar choices moeda
+    class Meta:
+        verbose_name = "Conta"
+        verbose_name_plural = "Contas"
+
+class ContaCliente(models.Model):
+    conta = models.ForeignKey(Conta, on_delete=models.PROTECT)
+    cliente = models.CharField("Cliente", max_length=200)
