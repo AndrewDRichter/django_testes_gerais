fa9f67e5625f16340d00127f40ab62bf7d89dc7e
AndrewDRichter <andrewrichter1@hotmail.com>
2024-05-09 16:51:51 -0400

    very very small test

--- base/views.py
+++ base/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Bem vindo a Home")
+
+def teste(request):
+    return HttpResponse("<h1>Testando rotas</h1></ br><ul><li>Item 1</li></ul>")
